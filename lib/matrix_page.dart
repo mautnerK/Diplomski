5da7a15dc5ec232f65078e2c3f2a14be39e57105
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-06-27 21:30:32 +0200

    fixed controllers, added checks for buttons

--- lib/matrix_page.dart
+++ lib/matrix_page.dart
@@ -18,100 +18,15 @@ class _MatrixPageState extends State<MatrixPage> {
   List<Widget> rows = [];
   double marginSize = 0;
   List<TextEditingController> controllers = [];
-  final c0 = TextEditingController(text: '');
-  int nr=0;
-  createRow() {
-    nr++;
-      for(int i = 0; i < widget.columnNumber; i++) {
-        rowElements.add(matrixInput());
-      }
-  }
-   createControllers() {
-     final r1c1 = TextEditingController(text: '');
-     final r1c2 = TextEditingController(text: '');
-     final r1c3 = TextEditingController(text: '');
-     final r1c4 = TextEditingController(text: '');
-     final r1c5 = TextEditingController(text: '');
-     final r2c1 = TextEditingController(text: '');
-     final r2c2 = TextEditingController(text: '');
-     final r2c3 = TextEditingController(text: '');
-     final r2c4 = TextEditingController(text: '');
-     final r2c5 = TextEditingController(text: '');
-     final r3c1 = TextEditingController(text: '');
-     final r3c2 = TextEditingController(text: '');
-     final r3c3 = TextEditingController(text: '');
-     final r3c4 = TextEditingController(text: '');
-     final r3c5 = TextEditingController(text: '');
-     final r4c1 = TextEditingController(text: '');
-     final r4c2 = TextEditingController(text: '');
-     final r4c3 = TextEditingController(text: '');
-     final r4c4 = TextEditingController(text: '');
-     final r4c5 = TextEditingController(text: '');
-     final r5c1 = TextEditingController(text: '');
-     final r5c2 = TextEditingController(text: '');
-     final r5c3 = TextEditingController(text: '');
-     final r5c4 = TextEditingController(text: '');
-     final r5c5 = TextEditingController(text: '');
-     controllers.add(r1c1);
-     controllers.add(r1c2);
-     controllers.add(r1c3);
-     controllers.add(r1c4);
-     controllers.add(r1c1);
-     controllers.add(r1c5);
-     controllers.add(r2c1);
-     controllers.add(r2c2);
-     controllers.add(r2c3);
-     controllers.add(r2c4);
-     controllers.add(r2c5);
-     controllers.add(r3c1);
-     controllers.add(r3c2);
-     controllers.add(r3c3);
-     controllers.add(r3c4);
-     controllers.add(r3c5);
-     controllers.add(r4c1);
-     controllers.add(r4c2);
-     controllers.add(r4c3);
-     controllers.add(r4c4);
-     controllers.add(r4c5);
-     controllers.add(r5c1);
-     controllers.add(r5c2);
-     controllers.add(r5c3);
-     controllers.add(r5c4);
-     controllers.add(r5c5);
-
-   }
-  createMatrix() {
-    createRow();
-    for(int i = 0; i < widget.rowNumber; i++) {
-      rows.add(Row(
-          children: rowElements
-      ));
-    }
-  }
-
-
-
-  createMarginSize(width) {
-    switch(widget.columnNumber) {
-      case 1: {marginSize = width*0.41;}
-      break;
-      case 2: {marginSize = width*0.35;;}
-      break;
-      case 3: {marginSize = width*0.26;;}
-      break;
-      case 4: {marginSize = width*0.17;;}
-      break;
-      case 5: {marginSize = width*0.08;;}
-      break;
-    }
-  }
+  int nr = -1;
+  bool isButtonDisabled = true;
 
   @override
   void initState() {
     super.initState();
     createControllers();
     createMatrix();
-
+    disableButton();
   }
 
   @override
@@ -141,19 +56,19 @@ class _MatrixPageState extends State<MatrixPage> {
           ),
           const SizedBox(height: 50),
           ElevatedButton(
-              onPressed: (){},
+              onPressed: isButtonDisabled ? () => disableButton : null,
               style: style,
               child: const Text('REGULARITY') ),
+          const SizedBox(height: 5),
+          ElevatedButton(
+              onPressed: isButtonDisabled ? () => disableButton : null,
+              style: style,
+              child: const Text('INVERSE')),
           const SizedBox(height: 5),
           ElevatedButton(
               onPressed: (){},
               style: style,
               child: const Text('TRANSPOSITION')),
-          const SizedBox(height: 5),
-          ElevatedButton(
-              onPressed: (){},
-              style: style,
-              child: const Text('INVERSE')),
           const SizedBox(height: 5),
           ElevatedButton(
               onPressed: (){},
@@ -174,10 +89,7 @@ class _MatrixPageState extends State<MatrixPage> {
     );
   }
 
-  Widget matrixInput(){
-
-
-
+  Widget matrixInput(int nr){
     return Container(
       margin: const EdgeInsets.all(3),
       child: MatrixInput(
@@ -195,9 +107,101 @@ class _MatrixPageState extends State<MatrixPage> {
           )
       ),
     );
+  }
 
-     }
+  createRow() {
+    for(int i = 0; i < widget.columnNumber; i++) {
+      nr++;
+      rowElements.add(matrixInput(nr));
     }
+  }
+
+  createMatrix() {
+    createRow();
+    for(int i = 0; i < widget.rowNumber; i++) {
+      rows.add(Row(
+          children: rowElements
+      ));
+      rowElements = [];
+      createRow();
+    }
+
+  }
+
+  disableButton() {
+    if (widget.rowNumber != widget.columnNumber) {
+      isButtonDisabled = false;
+    }
+  }
+
+  createMarginSize(width) {
+    switch(widget.columnNumber) {
+      case 1: {marginSize = width*0.41;}
+      break;
+      case 2: {marginSize = width*0.35;;}
+      break;
+      case 3: {marginSize = width*0.26;;}
+      break;
+      case 4: {marginSize = width*0.17;;}
+      break;
+      case 5: {marginSize = width*0.08;;}
+      break;
+    }
+  }
+
+  createControllers() {
+    final controller1 = TextEditingController(text: '1');
+    final controller2 = TextEditingController(text: '2');
+    final controller3 = TextEditingController(text: '3');
+    final controller4 = TextEditingController(text: '4');
+    final controller5 = TextEditingController(text: '5');
+    final controller6 = TextEditingController(text: '6');
+    final controller7 = TextEditingController(text: '7');
+    final controller8 = TextEditingController(text: '8');
+    final controller9 = TextEditingController(text: '9');
+    final controller10 = TextEditingController(text: '10');
+    final controller11 = TextEditingController(text: '11');
+    final controller12 = TextEditingController(text: '12');
+    final controller13 = TextEditingController(text: '13');
+    final controller14 = TextEditingController(text: '14');
+    final controller15 = TextEditingController(text: '15');
+    final controller16 = TextEditingController(text: '16');
+    final controller17 = TextEditingController(text: '17');
+    final controller18 = TextEditingController(text: '18');
+    final controller19 = TextEditingController(text: '19');
+    final controller20 = TextEditingController(text: '20');
+    final controller21 = TextEditingController(text: '21');
+    final controller22 = TextEditingController(text: '22');
+    final controller23 = TextEditingController(text: '23');
+    final controller24 = TextEditingController(text: '24');
+    final controller25 = TextEditingController(text: '25');
+    controllers.add(controller1);
+    controllers.add(controller2);
+    controllers.add(controller3);
+    controllers.add(controller4);
+    controllers.add(controller5);
+    controllers.add(controller6);
+    controllers.add(controller7);
+    controllers.add(controller8);
+    controllers.add(controller9);
+    controllers.add(controller10);
+    controllers.add(controller11);
+    controllers.add(controller12);
+    controllers.add(controller13);
+    controllers.add(controller14);
+    controllers.add(controller15);
+    controllers.add(controller16);
+    controllers.add(controller17);
+    controllers.add(controller18);
+    controllers.add(controller19);
+    controllers.add(controller20);
+    controllers.add(controller21);
+    controllers.add(controller22);
+    controllers.add(controller23);
+    controllers.add(controller24);
+    controllers.add(controller25);
+  }
+}
 
 
 
